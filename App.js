30f6541b1452c67e9dd550412cbf0a32a06cb147
m4-programmer <miraboy13@gmail.com>
2022-08-15 21:53:15 +0100

    First Release of TeslaApp Clone

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import CarList from './Components/CarList';
+import Header from "./Components/Header";
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <CarList />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
